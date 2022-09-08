445eef3b4b5f673ee132e5a5486048a557082eb6
Wendel-D <wendeldaniel263@gmail.com>
2022-09-08 16:26:05 -0300

    requisito 3 ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default class Home extends Component {
   render() {
     const { listaProdutos } = this.props;
     return (
+      <main>
       <div>
         {
           listaProdutos.length === 0 && (
@@ -16,6 +18,14 @@ export default class Home extends Component {
             </h1>)
         }
       </div>
+      <div>
+        <p>
+          <Link to="/ShopCart" data-testid="shopping-cart-button">
+            Carrinho de compras
+          </Link>
+        </p>
+      </div>
+      </main>
     );
   }
 }
