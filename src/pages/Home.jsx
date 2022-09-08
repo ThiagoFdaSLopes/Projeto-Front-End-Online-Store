b44c641e7930f3f91104bfe5cc89d4a454aaf349
Augusto Duarte <augustodepauliduartesjrp@gmail.com>
2022-09-08 17:00:15 -0300

    Requisito 4 correto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const { categoriesList } = this.state;
+    const data = await getCategories();
+    this.setState({ categoriesList: data });
+    console.log(categoriesList);
+  };
+
   render() {
     const { listaProdutos } = this.props;
+    const { categoriesList } = this.state;
     return (
       <main>
         <div>
@@ -25,6 +45,22 @@ export default class Home extends Component {
             </Link>
           </p>
         </div>
+
+        <ul>
+          {
+            categoriesList.map((elem) => (
+              <div key={ elem.id }>
+                <button
+                  data-testid="category"
+                  type="button"
+                  name={ elem.id }
+                >
+                  { elem.name }
+                </button>
+              </div>
+            ))
+          }
+        </ul>
       </main>
     );
   }
