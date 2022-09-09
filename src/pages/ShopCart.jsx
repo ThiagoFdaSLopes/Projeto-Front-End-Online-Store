f00ba1b6e230147cef0433051dca42fa145fa700
Augusto Duarte <augustodepauliduartesjrp@gmail.com>
2022-09-09 13:22:04 -0300

    Tentando Requisito 8

--- src/pages/ShopCart.jsx
+++ src/pages/ShopCart.jsx
@@ -5,14 +5,46 @@ export default class ShopCart extends Component {
     carrinho: [],
   };
 
+  componentDidMount() {
+    const local = JSON.parse(localStorage.getItem('cartItems')) || [];
+    this.setState({
+      carrinho: local,
+    });
+  }
+
   render() {
     const { carrinho } = this.state;
+    const filter = carrinho.filter((e, i) => carrinho.indexOf(e) === i);
+    console.log(filter);
+
     return (
       <div>
         <h1>Carrinho de compras</h1>
         {
           carrinho.length === 0
-          && <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+            ? <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+            : (
+              carrinho.map((e) => (
+                <>
+                  <div data-testid="product" key={ e.id }>
+                    <img src={ e.thumbnail } alt={ e.title } />
+                    <p>{e.title}</p>
+                    <p>{`Valor: ${e.price}`}</p>
+                  </div>
+
+                  <p>
+                    {
+                      carrinho.reduce((acc, elem) => {
+                        if (elem.id === e.id) {
+                          acc += 1;
+                        }
+                        return acc;
+                      }, 0)
+                    }
+                  </p>
+                </>
+              ))
+            )
         }
       </div>
     );
