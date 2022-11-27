b1e4effb08723a33536e061f65f1cbf17fe58360
Thiago <programadorthiagolopes@gmail.com>
2022-11-27 10:53:37 -0300

    fix(eslint): alterado formatador do projeto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Count from '../components/Count';
-import { getCategories, getProductsFromCategoryAndQuery,
-  setLocalItems } from '../services/api';
+import {
+  getCategories,
+  getProductsFromCategoryAndQuery,
+  setLocalItems,
+} from '../services/api';
 
 export default class Home extends Component {
   constructor() {
@@ -61,7 +64,10 @@ export default class Home extends Component {
   };
 
   categoryClick = async (event) => {
-    const produtos = await getProductsFromCategoryAndQuery(event.target.name, null);
+    const produtos = await getProductsFromCategoryAndQuery(
+      event.target.name,
+      null,
+    );
     this.setState({
       listProdutos: produtos.results,
       pesquisou: true,
@@ -87,77 +93,70 @@ export default class Home extends Component {
   };
 
   render() {
-    const { categoriesList,
-      textBusca, listProdutos, pesquisou, itensCartQT } = this.state;
+    const {
+      categoriesList, textBusca, listProdutos, pesquisou, itensCartQT } = this.state;
     return (
-      <main>
-        <div className="campoDeBusca">
-          <input
-            type="text"
-            name="textBusca"
-            value={ textBusca }
-            onChange={ this.handleChange }
-            data-testid="query-input"
-          />
-          <button
-            type="button"
-            data-testid="query-button"
-            onClick={ this.handleClick }
-          >
-            Pesquisar
-          </button>
-          <Count itensCartQT={ itensCartQT } />
+      <>
+        <header className="header-home">
+          <div className="campoDeBusca">
+            <input
+              type="text"
+              name="textBusca"
+              value={ textBusca }
+              onChange={ this.handleChange }
+              data-testid="query-input"
+            />
+            <button
+              type="button"
+              data-testid="query-button"
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+            <Count itensCartQT={ itensCartQT } />
+          </div>
+        </header>
+        <div>
+          {listProdutos.length === 0 && (
+            <h1 data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </h1>
+          )}
         </div>
         <div>
-          {
-            listProdutos.length === 0 && (
-              <h1
-                data-testid="home-initial-message"
-              >
-                Digite algum termo de pesquisa ou escolha uma categoria.
+          {pesquisou && listProdutos.length === 0 ? (
+            <p>Nenhum produto foi encontrado</p>
+          ) : (
+            listProdutos.map((e) => (
+              <div data-testid="product" key={ e.id }>
+                <div>
+                  {e.shipping.free_shipping && (
+                    <p data-testid="free-shipping">Frete grátis</p>
+                  )}
+                  <img src={ e.thumbnail } alt={ e.title } />
+                  <p>{e.title}</p>
+                  <p>{`Valor: ${e.price}`}</p>
+                  <Link
+                    data-testid="product-detail-link"
+                    to={ `/productdetails/${e.id}` }
+                  >
+                    Detalhes
+                  </Link>
+                </div>
 
-              </h1>)
-          }
-        </div>
-        <div>
-          {
-            pesquisou && listProdutos.length === 0
-              ? <p>Nenhum produto foi encontrado</p>
-              : (
-                listProdutos.map((e) => (
-                  <div data-testid="product" key={ e.id }>
-                    <div>
-                      {
-                        e.shipping.free_shipping
-                        && <p data-testid="free-shipping">Frete grátis</p>
-                      }
-                      <img src={ e.thumbnail } alt={ e.title } />
-                      <p>{e.title}</p>
-                      <p>{`Valor: ${e.price}`}</p>
-                      <Link
-                        data-testid="product-detail-link"
-                        to={ `/productdetails/${e.id}` }
-                      >
-                        Detalhes
-
-                      </Link>
-
-                    </div>
-
-                    <div>
-                      <button
-                        name={ e.id }
-                        type="button"
-                        data-testid="product-add-to-cart"
-                        onClick={ () => this.handleCart(e.id) }
-                      >
-                        Adicionar ao Carrinho!
-                      </button>
-                    </div>
-                  </div>
-                ))
-              )
-          }
+                <div>
+                  <button
+                    name={ e.id }
+                    type="button"
+                    data-testid="product-add-to-cart"
+                    onClick={ () => this.handleCart(e.id) }
+                  >
+                    Adicionar ao Carrinho!
+                  </button>
+                </div>
+              </div>
+            ))
+          )}
         </div>
         <div>
           <p>
@@ -168,22 +167,20 @@ export default class Home extends Component {
         </div>
 
         <ul>
-          {
-            categoriesList.map((elem) => (
-              <div key={ elem.id }>
-                <button
-                  data-testid="category"
-                  type="button"
-                  name={ elem.id }
-                  onClick={ this.categoryClick }
-                >
-                  { elem.name }
-                </button>
-              </div>
-            ))
-          }
+          {categoriesList.map((elem) => (
+            <div key={ elem.id }>
+              <button
+                data-testid="category"
+                type="button"
+                name={ elem.id }
+                onClick={ this.categoryClick }
+              >
+                {elem.name}
+              </button>
+            </div>
+          ))}
         </ul>
-      </main>
+      </>
     );
   }
 }
