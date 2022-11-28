3d950f904cadedf25419283536beba4ebdd162a5
Thiago <programadorthiagolopes@gmail.com>
2022-11-28 09:54:24 -0300

    featr(form): Finalizado edição do form

--- src/components/FormProductDetail.jsx
+++ src/components/FormProductDetail.jsx
@@ -8,98 +8,98 @@ export default class FormProductDetail extends Component {
       <div className="box-form">
         <p>Avaliações</p>
         <form className="form">
-          <input
-            data-testid="product-detail-email"
-            type="email"
-            name="emails"
-            id="email"
-            value={ emails }
-            placeholder="Email"
-            className="input-form-email"
-            onChange={ handleChange }
-          />
-
-          <label htmlFor="1">
+          <div className="rating">
             <input
-              className="star-rating"
-              type="radio"
-              name="stars"
-              id="1"
-              value="1"
-              data-testid="1-rating"
+              data-testid="product-detail-email"
+              type="email"
+              name="emails"
+              id="email"
+              value={ emails }
+              placeholder="Email"
+              className="input-form-email"
               onChange={ handleChange }
             />
-            1 ⭐
-          </label>
+            <label htmlFor="1">
+              <input
+                className="star-rating"
+                type="radio"
+                name="stars"
+                id="1"
+                value="1"
+                data-testid="1-rating"
+                onChange={ handleChange }
+              />
+              1
+            </label>
 
-          <label htmlFor="2">
-            <input
-              type="radio"
-              name="stars"
-              id="2"
-              value="2"
-              data-testid="2-rating"
-              onChange={ handleChange }
-            />
-            2 ⭐⭐
-          </label>
+            <label htmlFor="2">
+              <input
+                type="radio"
+                name="stars"
+                id="2"
+                value="2"
+                data-testid="2-rating"
+                onChange={ handleChange }
+              />
+              2
+            </label>
 
-          <label htmlFor="3">
-            <input
-              type="radio"
-              name="stars"
-              id="3"
-              value="3"
-              data-testid="3-rating"
-              onChange={ handleChange }
-            />
-            3 ⭐⭐⭐
-          </label>
+            <label htmlFor="3">
+              <input
+                type="radio"
+                name="stars"
+                id="3"
+                value="3"
+                data-testid="3-rating"
+                onChange={ handleChange }
+              />
+              3
+            </label>
 
-          <label htmlFor="4">
-            <input
-              type="radio"
-              name="stars"
-              id="4"
-              value="4"
-              data-testid="4-rating"
-              onChange={ handleChange }
-            />
-            4 ⭐⭐⭐⭐
-          </label>
+            <label htmlFor="4">
+              <input
+                type="radio"
+                name="stars"
+                id="4"
+                value="4"
+                data-testid="4-rating"
+                onChange={ handleChange }
+              />
+              4
+            </label>
 
-          <label htmlFor="5">
-            <input
-              type="radio"
-              name="stars"
-              id="5"
-              value="5"
-              data-testid="5-rating"
-              onChange={ handleChange }
-            />
-            5 ⭐⭐⭐⭐⭐
-          </label>
-
+            <label htmlFor="5">
+              <input
+                type="radio"
+                name="stars"
+                id="5"
+                value="5"
+                data-testid="5-rating"
+                onChange={ handleChange }
+              />
+              5
+            </label>
+          </div>
           <textarea
             name="comentarios"
             id="comentario"
             rows="20"
             cols="60"
+            className="mensagem"
             value={ comentarios }
             placeholder="Mensagem(Opcional)"
             data-testid="product-detail-evaluation"
             onChange={ handleChange }
           />
-
-          <button
-            type="button"
-            data-testid="submit-review-btn"
-            onClick={ submitReview }
-          >
-            Avaliar
-          </button>
         </form>
-
+        <button
+          type="button"
+          data-testid="submit-review-btn"
+          className="submit-review"
+          onClick={ submitReview }
+        >
+          Avaliar
+        </button>
         { isInvalid && <p data-testid="error-msg">Campos inválidos</p> }
       </div>
     );
